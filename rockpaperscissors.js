0739e903fc259fb0eca56505cc0cef977b10545c
Afifa Haque <ahaqu@MacBook-Air.local>
2025-05-08 19:37:35 +1000

    Code in round function and game playing function.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -17,3 +17,45 @@ function getHumanChoice(){
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(getComputerChoice, getHumanChoice){
+    let humanChoice = humanChoice.toLowerCase();
+    if (humanChoice == computerChoice){
+        alert("Draw. Nobody beats each other.")
+    } else if (humanChoice=='rock' && computerChoice=='scissors'){
+        alert("You win! Rock beats scissors.");
+        ++humanScore;
+    } else if (humanChoice=='scissors' && computerChoice=='paper'){
+        alert("You win! Scissors beats paper.");
+        ++humanScore;
+    } else if (humanChoice =='paper' && computerChoice=='rock'){
+        alert("You win! Paper beats rock.");
+        ++humanScore;
+    } else if (humanChoice=='scissors' && computerChoice=='rock'){
+        alert("You lose! Rock beats scissors.")
+        ++computerScore;
+    } else if (humanChoice=='rock' && computerChoice=='paper'){
+        alert("You lose! Paper beats rock.")
+        ++computerScore;
+    }else if (humanChoice=='paper' && computerChoice=='scissors'){
+        alert("You lose! Scissors beats paper.")
+        ++computerScore;
+    }
+}
+
+function playGame(){
+    let count = 0;
+    while(count < 5){
+        playRound();
+        ++count;
+    }
+    if (computerScore == humanScore){
+        alert("Draw. Nobody wins the game.");
+    } else if (computerScore < humanScore){
+        alert("You win the game!");
+    } else {
+        alert("You lose the game!");
+    }
+}
+
+playGame();
